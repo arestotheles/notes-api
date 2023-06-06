6b3e1be5d717df8fc39c2005969c570e9424ba62
Hebert Santoso <santoso.hebert@gmail.com>
2023-06-07 02:23:46 +0700

    edit and delete function

--- src/services/inMemory/NotesService.js
+++ src/services/inMemory/NotesService.js
@@ -35,5 +35,34 @@ class NotesService {
         }
         return note;
     }
-}
 
+    editNoteById(id, {title,body,tags}){
+        const index = this._notes.findIndex((note) => note.id === id);
+
+        if(index === -1){
+            throw new Error('Gagal memperbarui catatan. Id tidak ditemukan');
+
+        }
+
+        const updatedAt = new Date().toISOString();
+
+        this._notes[index] = {
+            ...this._notes[index],
+            title,
+            tags,
+            body,
+            updatedAt,
+        };
+    }
+
+    deleteNoteById(id){
+        const index = this._notes.findIndex((note) => note.id === id);
+        if(index === -1){
+            throw new Error('Catatan gagal dihapus. Id tidak ditemukan');
+        }
+
+        this._notes.splice(index,1);
+    }
+
+}
+module.exports=NotesService;
